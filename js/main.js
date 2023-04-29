aa0da72fde8cfe3e47c062530944c7e8e15f8657
Collin Kravig <ckravig@gmail.com>
2023-04-29 17:36:34 -0500

    added camera movement

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ import MouseMeshInteraction from './three_mmi';
 // Import Topic Box object
 import createTopicBox from './topicBox';
 
+// Info View
+let infoView = false;
+
 // Debug const
 const debug = false;
 
@@ -26,7 +29,7 @@ const renderer = new THREE.WebGLRenderer({
   alpha: true,
 });
 
-// pass threejs scene and camera
+// pass threejs scene and camera to mmi
 const mmi = new MouseMeshInteraction(scene, camera);
 
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -82,39 +85,50 @@ scene.add(pointLight, ambientLight);
 
   // ^Gradient Background^ ---------------------------------------------------
 
-  // repairBox object -------------------------------------------------
+  // topicBox1 object -------------------------------------------------
 
-const repairBox = createTopicBox('/images/RightToRepair.jpg');
+const topicBox1 = createTopicBox('/images/RightToRepair.jpg');
+const topicBox2 = createTopicBox('/images/Recycle-Logo.jpg');
 
-scene.add(repairBox);
+topicBox1.position.z = -5;
+topicBox1.position.x = 0;
 
-repairBox.position.z = -5;
-repairBox.position.x = 0;
+topicBox2.position.z = -5;
+topicBox2.position.x = 5;
+
+scene.add(topicBox1);
+scene.add(topicBox2);
+
+
 
 const closeButton = document.getElementById('infoBoxCloseButton');
 const infoBox = document.getElementById('infoBox');
   
 closeButton.addEventListener('click', () => {
 	infoBox.style.visibility = 'hidden';
+  infoView = false;
+  console.log('infoView:', infoView);
 });
 
 if (debug) {
-  const repairBoxDebug = document.createElement('div');
-  repairBoxDebug.setAttribute('id', 'repairBoxDebug');
-  repairBoxDebug.innerHTML = 'Right to Repair';
+  const topicBox1Debug = document.createElement('div');
+  topicBox1Debug.setAttribute('id', 'topicBox1Debug');
+  topicBox1Debug.innerHTML = 'Right to Repair';
 
   // Add the div to an existing element with id "main"
-  document.getElementById('main').appendChild(repairBoxDebug);
+  document.getElementById('main').appendChild(topicBox1Debug);
 }
 
 // create a handler for when user clicks on a mesh with the name 'my_interactable_mesh'
-mmi.addHandler('topicBox', 'click', function(repairBox) {
+mmi.addHandler('topicBox', 'click', function(mesh) {
   console.log('interactable mesh has been clicked!');
-  console.log(repairBox);
+  console.log(topicBox1);
   infoBox.style.visibility = 'visible';
+  infoView = true;
+  console.log('infoView:', infoView);
 }); 
 
-  // ^repairBox object^ -------------------------------------------------
+  // ^topicBox1 object^ -------------------------------------------------
 
   // Scroll Animation -------------------------------------------------
 
@@ -124,8 +138,8 @@ mmi.addHandler('topicBox', 'click', function(repairBox) {
   // moon.rotation.y += 0.075;
   // moon.rotation.z += 0.05;
 
-  // repairBox.rotation.y += 0.01;
-  // repairBox.rotation.z += 0.01;
+  // topicBox1.rotation.y += 0.01;
+  // topicBox1.rotation.z += 0.01;
 
   //   camera.position.z = t * -0.01;
   //   camera.position.x = t * -0.0002;
@@ -137,12 +151,84 @@ mmi.addHandler('topicBox', 'click', function(repairBox) {
 
   // ^Scroll Animation^ -------------------------------------------------
 
-  // Topic Box Mouse Drag -------------------------------------------------
-  // Add event listeners for mouse events on repairBox object 
+// Topic Box Mouse Drag -------------------------------------------------
+// Add event listeners for mouse events on topicBox1 object 
+let isMouseDown = false;
+let previousMousePosition = { x: 0, y: 0 };
+let currentRotationSpeed = { x: 0, y: 0 };
+const momentumDampingFactor = 0.99;
+
+mmi.addHandler('topicBox', 'mousedown', function(mesh) {
+  // when mouse button is pressed down on the mesh, start tracking mouse movement
+  isMouseDown = true;
   
-// let isDragging = false;
-// let previousMousePosition = { x: 0, y: 0 };
-// let currentRotationSpeed = { x: 0, y: 0 };
+  document.addEventListener('mousemove', function(event) {
+    if (isMouseDown) {
+      onMouseMove(event, mesh);
+    }
+  });
+}); 
+
+// add a mouseup event listener to stop tracking mouse movement and rotation
+document.addEventListener('mouseup', function(event) {
+  isMouseDown = false;
+});
+
+// define a function to handle mouse movement
+function onMouseMove(event, mesh) {
+  if (isMouseDown) {
+    const deltaMove = {
+      x: event.clientX - previousMousePosition.x,
+      y: event.clientY - previousMousePosition.y,
+    };
+    currentRotationSpeed.x += deltaMove.y * 0.01;
+    currentRotationSpeed.y += deltaMove.x * 0.01;
+
+    const distance = Math.sqrt(deltaMove.x ** 2 + deltaMove.y ** 2);
+    if (distance > 2) { // add a threshold to reduce jittering
+      mesh.rotation.x = currentRotationSpeed.x;
+      mesh.rotation.y = currentRotationSpeed.y;
+    }
+  } else {
+    // decay current rotation speed over time
+    currentRotationSpeed.x *= momentumDampingFactor;
+    currentRotationSpeed.y *= momentumDampingFactor;
+    
+    // stop updating rotation if current rotation speed becomes negligible
+    if (Math.abs(currentRotationSpeed.x) < 0.01 && Math.abs(currentRotationSpeed.y) < 0.01) {
+      currentRotationSpeed = { x: 0, y: 0 };
+    }
+    
+    mesh.rotation.x = currentRotationSpeed.x;
+    mesh.rotation.y = currentRotationSpeed.y;
+  }
+  previousMousePosition = {
+    x: event.clientX,
+    y: event.clientY,
+  };
+}
+
+// define a function to update rotation with momentum
+function updateRotation(mesh) {
+  if (!isMouseDown) {mesh.rotation.y += 0.005;}
+  
+  // mesh.rotation.y += currentRotationSpeed.y * 0.01;
+  // mesh.rotation.x += currentRotationSpeed.x * 0.01;
+  // currentRotationSpeed.x *= momentumDampingFactor;
+  // currentRotationSpeed.y *= momentumDampingFactor;
+  // if (Math.abs(currentRotationSpeed.x) < 0.01 && Math.abs(currentRotationSpeed.y) < 0.01) {
+  //   currentRotationSpeed = { x: 0, y: 0 };
+  // }
+
+  if (!isMouseDown) {
+    topicBox1.rotation.y += 0.005;
+    if(topicBox1.rotation.x > 0) {
+      topicBox1.rotation.x += -0.01;
+    }else if(topicBox1.rotation.x < 0) {
+      topicBox1.rotation.x += 0.01;
+    }
+  }
+}
 
 // renderer.domElement.addEventListener('mousedown', event => {
 //   mouse.x (e.clientX / window.innerWidth) *2-1;
@@ -151,31 +237,46 @@ mmi.addHandler('topicBox', 'click', function(repairBox) {
 //   plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
 //   raycaster.setFromCamera(mouse, camera);
 //   raycaster.ray.intersectPlane(plane, intersectionPoint);
-//   // isDragging = true;
+//   isDragging = true;
 // });
 
-//   renderer.domElement.addEventListener('click', event => {
-//     repairBox.position
-//   });
+  // renderer.domElement.addEventListener('click', event => {
+  //   topicBox1.position
+  // });
 
-//   renderer.domElement.addEventListener('mousemove', event => {
-//     if (isDragging) {
-//       const deltaMove = {
-//         x: event.clientX - previousMousePosition.x,
-//         y: event.clientY - previousMousePosition.y,
-//       };
-//       currentRotationSpeed.x = deltaMove.y * 0.01;
-//       currentRotationSpeed.y = deltaMove.x * 0.01;
-//     }
-//     previousMousePosition = {
-//       x: event.clientX,
-//       y: event.clientY,
-//     };
-//   });
+  // mmi.addHandler('topicBox', 'mousemove', function(mesh) {
+  //   if (isDragging) {
+  //     const deltaMove = {
+  //       x: event.clientX - previousMousePosition.x,
+  //       y: event.clientY - previousMousePosition.y,
+  //     };
+  //     currentRotationSpeed.x = deltaMove.y * 0.01;
+  //     currentRotationSpeed.y = deltaMove.x * 0.01;
+  //   }
+  //   previousMousePosition = {
+  //     x: event.clientX,
+  //     y: event.clientY,
+  //   };
+  // }); 
 
-//   renderer.domElement.addEventListener('mouseup', event => {
-//     isDragging = false;
-//   });
+  // renderer.domElement.addEventListener('mousemove', event => {
+  //   if (isDragging) {
+  //     const deltaMove = {
+  //       x: event.clientX - previousMousePosition.x,
+  //       y: event.clientY - previousMousePosition.y,
+  //     };
+  //     currentRotationSpeed.x = deltaMove.y * 0.01;
+  //     currentRotationSpeed.y = deltaMove.x * 0.01;
+  //   }
+  //   previousMousePosition = {
+  //     x: event.clientX,
+  //     y: event.clientY,
+  //   };
+  // });
+
+  // renderer.domElement.addEventListener('mouseup', event => {
+  //   isDragging = false;
+  // });
 
 // ^Topic Box Mouse Drag^ -------------------------------------------------
 
@@ -188,27 +289,22 @@ function animate() {
   // torus.rotation.y += 0.005;
   // torus.rotation.z += 0.01;
 
-  repairBox.rotation.y += 0.01;
+  // topicBox1.rotation.y += 0.005;
 
-  // repairBox animation loop
+  updateRotation(topicBox1);
+
+  // topicBox1 animation loop
   // const dragFactor = 0.99;
-  // repairBox.rotation.x += currentRotationSpeed.x;
-  // repairBox.rotation.y += currentRotationSpeed.y;
+  // topicBox1.rotation.x += currentRotationSpeed.x;
+  // topicBox1.rotation.y += currentRotationSpeed.y;
   // currentRotationSpeed.x *= dragFactor;
   // currentRotationSpeed.y *= dragFactor;
 
   if (debug) {
-    repairBoxDebug.innerHTML = `X axis: ${repairBox.rotation.x}`;
+    topicBox1Debug.innerHTML = `X axis: ${topicBox1.rotation.x}`;
   }
 
-  // if (!isDragging) {
-  //   repairBox.rotation.y += 0.01;
-  //   if(repairBox.rotation.x > 0) {
-  //     repairBox.rotation.x += -0.01;
-  //   }else if(repairBox.rotation.x < 0) {
-  //     repairBox.rotation.x += 0.01;
-  //   }
-  // }
+
 
   // moon.rotation.x += 0.005;
 
@@ -236,3 +332,24 @@ function animate() {
 function toRadians(degrees) {
   return degrees * (Math.PI / 180);
 }
+
+// ^ Helper functions ^ -------------------------------------------------
+
+// Key Controls -------------------------------------------------
+
+document.addEventListener('keydown', function(event) {
+  if (infoView === false) {
+    
+    if (event.key === 'ArrowRight') {
+      camera.position.x += 5;
+    }
+  
+    if (event.key === 'ArrowLeft') {
+      camera.position.x += -5;
+    }
+
+  }
+  console.log('infoView:', infoView);
+});
+
+// ^ Key Controls ^ -------------------------------------------------
--- js/topicBox.js
+++ js/topicBox.js
@@ -6,6 +6,11 @@ export default function createTopicBox(topicBoxMapPath) {
   const geometry = new THREE.BoxGeometry(1, 1, 1);
   const material = new THREE.MeshBasicMaterial({ map: topicBoxMapTexture });
   const mesh = new THREE.Mesh(geometry, material);
+  
+  // add rotationSpeed property
+  mesh.rotationSpeed = { x: 0.005, y: 0 };
+
+  // add a name property
   mesh.name = 'topicBox';
   
   
